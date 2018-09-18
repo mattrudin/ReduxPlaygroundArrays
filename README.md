89be4523d04475fecf574dca00c31067ef32495e
mattrudin <matthias.rudin@live.com>
2018-09-18 12:24:40 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,18 @@
+# ReduxPlaygroundArrays
+Playground app with react and redux
+
+
+## Index
+
+[1. Instructions](#1-instructions/) 
+
+[2. Lessons learned](#2-lessons-learned)
+
+
+
+## 1. Instructions
+
+
+
+## 2. Lessons learned
+### SUBTILE
---
+++ src/components/List/list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './List.css';
+import COMPONENT from '../COMPONENT/COMPONENT';
+
+class List extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
+	}
+	render() {
+		return(
+			<div className="List">
+				
+			</div>
+		);
+	}
+}
+
+export default List;
